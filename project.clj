a57ac18c3b6a1b81dff29f937e2c50834abcbc56
Joe Van Dyk <joe@fixieconsulting.com>
2012-12-27 12:36:12 -0800

    wip

--- project.clj
+++ project.clj
@@ -3,5 +3,22 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"] [clj-http "0.6.3"] [org.clojure/data.xml "0.0.6"]]
-  :main rooster-shipment-tracker.core)
+
+  :dependencies [
+                 [org.clojure/clojure "1.4.0"] 
+                 [clj-http "0.6.3"] 
+                 [org.clojure/data.xml "0.0.6"]
+                 [compojure "1.1.3"]
+                ]
+
+
+  :profiles { 
+    :dev 
+      {:dependencies [
+      ]}}
+
+  :ring {:handler rooster-shipment-tracker.core/app}
+  :plugins [[lein-ring "0.7.5"]]
+
+  :main rooster-shipment-tracker.core
+)
--- src/rooster_shipment_tracker/core.clj
+++ src/rooster_shipment_tracker/core.clj
@@ -1,6 +1,8 @@
 (ns rooster-shipment-tracker.core
   (:require [clj-http.client :as http])
   (:require [clojure.data.xml :as xml])
+  (:require [compojure.core :as c])
+  (:require [compojure.handler :as c.handler])
   (:gen-class))
 
 (defn fetch [url] 
@@ -9,9 +11,18 @@
 (defn parse [xml] 
   (xml/parse (java.io.StringReader. xml)))
 
+(c/defroutes app-routes 
+             (c/GET "/" [] "hooray!")
+             (c/GET "/fetch" [url] (fetch url ))
+             )
+
+
+(def app (c.handler/site app-routes))
+
 (defn -main
   "Given a URL, downloads and parses it."
   [url]
   (println (parse (fetch url)))
   ; same as
   (-> url fetch parse println))
+
